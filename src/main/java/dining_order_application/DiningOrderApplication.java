5327ca8951474f1c5035bb13f5049e2127e72d8e
Akash Adiga <akashadigasc@gmail.com>
2025-02-20 09:09:54 +0530

    dining project is added

---
+++ src/main/java/dining_order_application/DiningOrderApplication.java
@@ -0,0 +1,13 @@
+package dining_order_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiningOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dining_order_application/DiningOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package dining_order_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
